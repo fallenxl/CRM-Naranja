6c6c4ef196874f96a41ed4ca0c19af3b634c7315
FallEn <axlsntz.dev@gmail.com>
2024-07-23 10:16:53 -0600

    Added changes

--- client/src/utils/charts.ts
+++ client/src/utils/charts.ts
@@ -1,26 +1,35 @@
 export function getStatusColor(status: string) {
-  switch (status.toLowerCase()) {
-    case 'por asignar':
-        // gray
-        return '#BDBDBD';
-    case 'oportunidad de venta futura':
-        // purple
-        return '#7E57C2';
-    case 'pendiente de llamar':
-        return '#98ff96'
-    case 'a contactar':
-        return '#5ccb5f'
-    case 'precalificar buró':
-        return '#009929'
-    case 'precalificar banco':
-         return '#006414'
-    case 'por asignar proyecto':
-        return '#efb810'
-    case 'por asignar modelo de casa':
-        return '#efb810'
-    case 'anulado':
-        return '#BDBDBD'
-    default:
-        return '#54b2ff'
-  }
+    switch (status.toLowerCase()) {
+        case 'por asignar':
+            // gray
+            return '#BDBDBD';
+        case 'oportunidad de venta futura':
+            // purple
+            return '#7E57C2';
+        case 'pendiente de llamar':
+            return '#98ff96'
+        case 'a contactar':
+            return '#5ccb5f'
+        case 'precalificar buró':
+            return '#009929'
+        case 'precalificar banco':
+            return '#006414'
+        case 'por asignar proyecto':
+            return '#efb810'
+        case 'por asignar modelo de casa':
+            return '#efb810'
+        case 'anulado':
+            return '#BDBDBD'
+        case 'liquidado':
+            // red
+            return '#FF5252';
+        case 'disponible':
+            // green
+            return '#4CAF50';
+        case 'reservado':
+            // orange
+            return '#FF9800';
+        default:
+            return '#54b2ff'
+    }
 }
